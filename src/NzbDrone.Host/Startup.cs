d09bff26e2a5c77a6726bc4d1034081496e83617
Terry R <terry.rankine@gmail.com>
2026-03-19 18:22:47 +0800

    Revert "Merge pull request #6 from terryrankine/fix/add-security-headers"
    This reverts commit e6f053525243cdc98412f3561241f9cbeb11bb72, reversing
    changes made to d44623cc30658e79cc283f954214bd36d2d02400.

--- src/NzbDrone.Host/Startup.cs
+++ src/NzbDrone.Host/Startup.cs
@@ -330,7 +330,6 @@ namespace NzbDrone.Host
             }
 
             app.UseForwardedHeaders();
-            app.UseMiddleware<SecurityHeadersMiddleware>();
             app.UseMiddleware<LoggingMiddleware>();
             app.UsePathBase(new PathString(configFileProvider.UrlBase));
             app.UseExceptionHandler(new ExceptionHandlerOptions
